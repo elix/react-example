9ef63c73b12486c43d4bb2dec3a5065675c16f18
Jan Miksovsky <jan@quickui.org>
2017-02-02 17:08:44 -0800

    Add polyfills. Add use of a custom element (not yet in package.json).

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ListBox from 'elix/elements/demos/src/ListBox';
 
 ReactDOM.render(
   <div>
-    <h1>Sample app</h1>
-    <p>
-      Hello, world!
-    </p>
+    <h1>React app with an Elix list box</h1>
+    <sample-list-box>
+      <div>Acai</div>
+      <div>Acerola</div>
+      <div>Apple</div>
+      <div>Apricot</div>
+      <div>Banana</div>
+      <div>Blackberry</div>
+      <div>Blueberry</div>
+      <div>Cantaloupe</div>
+      <div>Cherry</div>
+    </sample-list-box>
   </div>,
   document.querySelector('#root')
 );
