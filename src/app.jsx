682da8f85a85ef05475b01db08db2e40b91ec399
Jan Miksovsky <jan@quickui.org>
2017-02-02 17:24:03 -0800

    Render items.

--- src/app.jsx
+++ src/app.jsx
@@ -2,20 +2,53 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ListBox from 'elix/elements/demos/src/ListBox';
 
-ReactDOM.render(
-  <div>
-    <h1>React app with an Elix list box</h1>
-    <sample-list-box>
-      <div>Acai</div>
-      <div>Acerola</div>
-      <div>Apple</div>
-      <div>Apricot</div>
-      <div>Banana</div>
-      <div>Blackberry</div>
-      <div>Blueberry</div>
-      <div>Cantaloupe</div>
-      <div>Cherry</div>
-    </sample-list-box>
-  </div>,
-  document.querySelector('#root')
-);
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [
+        'Acai',
+        'Acerola',
+        'Apple',
+        'Apricot',
+        'Banana',
+        'Blackberry',
+        'Blueberry',
+        'Cantaloupe',
+        'Cherry'
+      ],
+      selectedIndex: 0
+    };
+  }
+
+  componentDidMount() {
+    this.refs.list.addEventListener('selected-index-changed', event => {
+      this.setState({
+        selectedIndex: event.detail.selectedIndex
+      });
+    });
+  }
+
+  render() {
+    const itemElements = this.state.items && this.state.items.map(item => (
+      <div key={item}>{item}</div>
+    ));
+    return (
+      <div>
+        <h1>React app with an Elix list box</h1>
+        <p>
+          Selection: {this.state.items[this.state.selectedIndex]}
+        </p>
+        <sample-list-box ref="list" selected-index={this.state.selectedIndex}>
+          {itemElements}
+        </sample-list-box>
+      </div>
+    );
+  }
+
+}
+
+
+ReactDOM.render(<App/>, document.querySelector('#root'));
